c95310fb1c300816b00990c5798ea65c0689ae03
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-03-03 23:52:45 +0900

    Create python_basic01.py

---
+++ python_basic/python_basic01.py
@@ -0,0 +1,20 @@
+print('Hello World')
+ a=10
+ print(a)
+ b=25
+ print(a+b)
+ sum = a + b +b
+ print(sum)
+ result= a/b
+ print(result)
+ a=23
+ b=4
+ print(a%b)
+ a='Hello World01'
+ b="Hello World02"
+ c='''Hello World03'''
+ d="""Hello World04""" 
+ print(a)
+ print(b)
+ print(c)
+ print(d)
