871c94f5ac28fa364de26bda3a9e841c8ff4713e
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-03-13 23:56:20 +0900

    Create python_basic07.py

---
+++ python_basic/python_basic07.py
@@ -0,0 +1,29 @@
+#append 함수
+#append(추가하다)
+a=["김이박","밥밥밥","컵컵컵"]
+a.append("가가가")
+print(a)
+#sort함수
+#리스트의 정렬
+#문자의 경우 가다나 혹은 알파벳 순서
+a=["세","사","나","가"]
+a.sort()
+print(a)
+a=["ば","け","ら","な"]
+a.sort()
+print(a)
+a=["o","r","q","a"]
+a.sort()
+print(a)
+#숫자의 경우킈 크기 순서로 정렬이된다.
+a=[3,6,8,1,9]
+a.sort()
+print(a)
+#reverse 함수
+#리스트 뒤집기
+a.reverse()
+print(a)
+#index 함수
+#위치 반환
+print(a.index(3))
+#오번이 있냐 ?? 어디에 있는지 인덱스 숫자로 알려줌
