9027c1201a74c53cc5751b02f99efc6a89dbfd88
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-04-03 11:08:17 +0900

    Update keras57_1_flow.py

--- keras41-60/keras57_1_flow.py
+++ keras41-60/keras57_1_flow.py
@@ -1,6 +1,7 @@
 # 이제까지는 이미지를 포러데엇 가져다가 수치화 시켰다. 
 # 하지만 수치로 제공되는 이미지 데이터도 잇다. 
 # 그것을 우리는 증폭을 할 것이다. 
+
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
@@ -9,15 +10,15 @@ import numpy as np
 train_datagen = ImageDataGenerator(
      rescale=1./255, 
     horizontal_flip=True, 
-    vertical_flip=True ,
+    #vertical_flip=True ,
     width_shift_range=0.1,
     height_shift_range=0.1, 
     rotation_range=5, 
     zoom_range=1.2, 
     shear_range= 0.7,
     fill_mode='nearest' 
-)
-augment_size = 100, # 
+) 
+augment_size = 100 
 print(x_train.shape) #(60000, 28, 28)
 print(x_train[0].shape) # (28, 28) 
 print(x_train[1].shape) # (28, 28)
@@ -57,7 +58,6 @@ for i in range(49) :
 plt.show()
 
 
-# 각종 키워드 이해하기 
 
-# 월요일에 수정 더 함 
-
+ 
+ 
