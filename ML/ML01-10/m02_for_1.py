32e3c4d92c56ed7fa11554be11dff19a46f9bf1a
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-04-14 19:34:45 +0900

    Create m02_for_1.py

---
+++ ML/ML01-10/m02_for_1.py
@@ -0,0 +1,18 @@
+# for i in 리스트 데이터 : 
+aaa =['a','b','c']
+for i in aaa : 
+    print(i)
+'''
+a
+b
+c
+'''
+for index,value in enumerate(aaa) : 
+    print(index,value)
+# index : 순서, value : 값 
+'''
+0 a
+1 b
+2 c
+'''
+# FOR 문에 들어갈 수 잇는건 이터레이터이면 되고 리스트는 이터레이터에 들어간다. 
