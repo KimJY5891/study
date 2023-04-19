62f28e67fcfc838244142a6ff738fd781493dff4
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-04-19 11:02:20 +0900

    삼중for ans

---
+++ ML/ML11-20/m18_pipline2_01_classifier.py
@@ -0,0 +1,58 @@
+# 삼중 포문!! 
+# 1. 데이터 
+# 2. 스케일
+# 3. 모델 
+
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, StandardScaler,MaxAbsScaler,RobustScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.datasets import load_wine, load_digits, load_breast_cancer, fetch_covtype, load_iris
+from sklearn.utils import all_estimators
+import warnings
+warnings.filterwarnings('ignore')
+
+dataset_list = [
+    load_wine(return_X_y=True),
+    load_digits(return_X_y=True),
+    load_breast_cancer(return_X_y=True),
+    load_iris(return_X_y=True)
+]
+data_list_name = ['아이리스',
+                  '캔서',
+                  '와인',
+                  '디지트']
+scaler_list = [MinMaxScaler(),StandardScaler(),MaxAbsScaler(),RobustScaler() ]
+for i,v in enumerate(dataset_list):
+    # 1. 데이터
+    x,y = v
+    # 2. 모델 구성
+    for j in scaler_list :
+        try:
+            scaler = j
+            x=scaler.fit_transform(x)
+            max_score = 0
+            allAlgoritms = all_estimators(type_filter='classifier')
+            for (name, algorithms) in allAlgoritms:
+                model = algorithms()
+                scores = cross_val_score(model, x, y)
+                results = round(np.mean(scores), 4)
+                print("==================================================")
+                print("데이터 : ", data_list_name[i])
+                print("모델 : ", name)
+                print("점수 : ", scores)
+                print("스케일러 : ", str(scaler))
+                if max_score < results :
+                    max_score = results
+                    max_name = name
+                    max_scaler = j
+        except:
+            continue
+        
+print("==================================================")
+print("최고모델 : ", max_name, max_score, str(max_scaler))
