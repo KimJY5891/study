eb832c7f54a8390024db02bd06fb87652b6027b0
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-04-20 18:17:03 +0900

    m05_kfold_05_fetch_covtype

---
+++ ML/ML01-10/m05_kfold_05_fetch_covtype.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.datasets import fetch_covtype
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.metrics import accuracy_score
+
+# 1. 데이터
+x, y = fetch_covtype(return_X_y=True)
+n_splits = 5
+kfold = KFold(n_splits = n_splits,
+      shuffle=True,  
+      random_state=123,
+)
+
+# 2. 모델
+from sklearn.svm import LinearSVC
+model = LinearSVC()
+
+#3, 4. 컴파일, 훈련 ,평가, 예측
+scores = cross_val_score(model,x,y,cv =kfold)
+print('acc : ',scores,'cross_val_score 평균 : ',round(np.mean(scores),4))
