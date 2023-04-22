70ea9ba733e866323fe0db63e6f09d48295d8c5c
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-04-22 15:20:16 +0900

    Update m04_all_estimators1.py

--- ML/ML01-10/m04_all_estimators1.py
+++ ML/ML01-10/m04_all_estimators1.py
@@ -8,6 +8,7 @@ warnings.filterwarnings('ignore')
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.utils import all_estimators
 import sklearn as sk
+print(sk.__version__) # 1.0.2
 
 
 # 1. 데이터
@@ -24,18 +25,19 @@ x_test = scaler.transform(x_test)
 
 # 2. 모델 구성
 
-model = RandomForestClassifier(n_jobs=4)
+# model = RandomForestClassifier(n_jobs=4)
 allAlgoritms = all_estimators(type_filter='regressor')
 #allAlgoritms = all_estimators(type_filter='classifier')
-print('allAlgoritms : ',allAlgoritms)
 
-print(sk.__version__) # 1.0.2
+print('allAlgoritms : ',allAlgoritms)
+#튜플 안에 첫번째는 스트링 형태의 모델, 두번째는 클래스로 정의된 모델
 print(len(allAlgoritms)) #55
 
+max_r2 = 0
+max_name = '바보'
 for (name, algoritm) in allAlgoritms :
     try: # 에외처리 
         model = algoritm()
-        model.fit(x_train,y_train)
         # 3. 훈련
         model.fit(x_train,y_train) # 모델 마다 형식이 다르다.
         # 4. 평가, 예측
@@ -48,8 +50,11 @@ for (name, algoritm) in allAlgoritms :
         # y_pred = model.predict(x_test)
         # r2_score=r2_score(y_test,y_pred)
         # print("r2_score : ",r2_score)    
-    except :  # 에러가 있을 때 실행하고 나서 다시 for문 실행
-        print(name,' : 에러 모델')#에러가 난 모델은 안에 파라미터 넣어줘야한다. 
+    except :  
+         #에러가 뜨면 except로 바로 넘어간다. 에러가 안뜨면 정상적으로 for문이 돌아감.
+        # 에러가 있을 때 실행하고 나서 다시 for문 실행
+        print(name,' : 에러 모델')
+        #에러가 난 모델은 안에 파라미터 넣어줘야하는 모델이다.
 print('===================================')
 print('최고 모델 : ',max_name, max_r2)
 print('===================================')
@@ -60,6 +65,4 @@ print('===================================')
 5번째부터 뭔가 넣어야 하는데 그렇지 않으면 오류가 남 
 해결 방법은 에러가 나는 모델을 하지 말아주세요라고 부탁하기 
 - 예외처리 
-
-
 '''
