d7609abaaaaf01005e7448f50551fb3b69159a96
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-04-25 12:46:11 +0900

    LDA_EVR shape와 cumsum 비교

--- ML/ML31-40/m37_LDA_EVR.py
+++ ML/ML31-40/m37_LDA_EVR.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris, load_breast_cancer, \
     load_digits, load_wine, fetch_covtype
-from tensorflow.keras.datasets import cifar100
 
 # 1. 데이터 
 datasets_list = [
@@ -24,25 +23,24 @@ datasets_name_list = [
     '와인',
     '패치콥프타입'
 ]
+lda = LinearDiscriminantAnalysis()
 for i,v  in enumerate(datasets_list) :
     x,y = v
-    lda = LinearDiscriminantAnalysis()
     x_lda = lda.fit_transform(x,y)
-    print(datasets_name_list[i],"x_lda.shape : ",x_lda.shape)
+    print(datasets_name_list[i],"의 x.shape :",x.shape,"-> x_lda.shape : ",x_lda.shape)
     lda_EVR = lda.explained_variance_ratio_
     cumsum = np.cumsum(lda_EVR)
     print(datasets_name_list[i],"의 cumsum : ",cumsum)
-    
 '''
-아이리스 x_lda.shape :  (150, 2)
+아이리스 의 x.shape : (150, 4) -> x_lda.shape :  (150, 2)
 아이리스 의 cumsum :  [0.9912126 1.       ]
-브래스트 캔서 x_lda.shape :  (569, 1)
+브래스트 캔서 의 x.shape : (569, 30) -> x_lda.shape :  (569, 1)
 브래스트 캔서 의 cumsum :  [1.]
-디짓트 x_lda.shape :  (1797, 9)
+디짓트 의 x.shape : (1797, 64) -> x_lda.shape :  (1797, 9)
 디짓트 의 cumsum :  [0.28912041 0.47174829 0.64137175 0.75807724 0.84108978 0.90674662
  0.94984789 0.9791736  1.        ]
-와인 x_lda.shape :  (178, 2)
+와인 의 x.shape : (178, 13) -> x_lda.shape :  (178, 2)
 와인 의 cumsum :  [0.68747889 1.        ]
-패치콥프타입 x_lda.shape :  (581012, 6)
+패치콥프타입 의 x.shape : (581012, 54) -> x_lda.shape :  (581012, 6)
 패치콥프타입 의 cumsum :  [0.72159835 0.89235761 0.94351071 0.9742032  0.99067616 1.        ]
 '''
