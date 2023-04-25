4a0b7c014e5ce656338d0b37f3cb7dd011c9d75a
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-04-25 09:22:57 +0900

    Create m31_PCA04_wine.py

---
+++ ML/ML31-40/m31_PCA04_wine.py
@@ -0,0 +1,67 @@
+### 실습 ### 
+#for문 써서 한 번에 돌리기
+# 기본 결과 : 
+# 차원 1개 축소 :
+# 차원 2개 축소 : 
+ 
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_breast_cancer, load_diabetes, load_wine
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+
+# 1. 데이터 
+datasets = load_wine()
+print(datasets.feature_names)
+
+x = datasets['data']
+y = datasets['target']
+print(x.shape,y.shape) #(442, 10) (442,)
+x_train, x_test,y_train,y_test = train_test_split(
+    x,y , train_size=0.8,random_state=123, shuffle=True,
+)
+# 2. 모델 
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor(random_state=123)
+# 3. 훈련 
+model.fit(x_train,y_train)
+# 4. 평가, 예측
+results = model.score(x_test,y_test)
+print("처음 결과 : ", results)
+print(len(x[0]))
+for i in range(x.shape[1]):
+    #print('i 값 : ',i)
+    # 1. 데이터 
+    datasets = load_wine()
+    x = datasets['data']
+    y = datasets['target']
+    pca = PCA(n_components=(i+1))
+    x = pca.fit_transform(x)
+    x_train, x_test,y_train,y_test = train_test_split(
+        x,y , train_size=0.8,random_state=123, shuffle=True,
+    )
+    # 2. 모델 
+    from sklearn.ensemble import RandomForestRegressor
+    model = RandomForestRegressor(random_state=123)
+    # 3. 훈련 
+    model.fit(x_train,y_train)
+    # 4. 평가, 예측
+    results = model.score(x_test,y_test)
+    print(i+1,"차원 결과 : ", results)
+'''
+처음 결과 :  0.9480835164835165
+13
+1 차원 결과 :  0.2537318681318683
+2 차원 결과 :  0.4280527472527472
+3 차원 결과 :  0.6609362637362637
+4 차원 결과 :  0.7709230769230769
+5 차원 결과 :  0.7389274725274726
+6 차원 결과 :  0.7591384615384615
+7 차원 결과 :  0.7634065934065934
+8 차원 결과 :  0.7571076923076923
+9 차원 결과 :  0.7645582417582417
+10 차원 결과 :  0.7495780219780219
+11 차원 결과 :  0.7413142857142858
+12 차원 결과 :  0.732646153846154
+13 차원 결과 :  0.7326769230769231
+'''
