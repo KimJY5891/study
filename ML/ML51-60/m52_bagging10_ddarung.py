9612b837730bf1ec0d91348c3761657e602c1431
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-05-01 19:54:22 +0900

    연습

---
+++ ML/ML51-60/m52_bagging10_ddarung.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_diabetes
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,r2_score
+
+# 1. 데이터 
+
+path = "./_data/dacon_ddarung/"
+train_csv=pd.read_csv(path + 'train.csv',index_col=0)
+test_csv=pd.read_csv(path + 'test.csv',index_col=0)
+####################################### 결측치 처리 #######################################
+train_csv = train_csv.dropna()
+####################################### 결측치 처리 #######################################
+x = train_csv.drop(['count'],axis=1)#
+y = train_csv['count']
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x,y,random_state=337,train_size=0.8,shuffle=True,
+    # stratify=y
+)
+scaler = StandardScaler()
+x_train =scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+# 2. 모델 
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, RandomForestRegressor, BaggingRegressor
+from sklearn.tree import DecisionTreeClassifier
+
+model = BaggingRegressor(
+# BaggingRegressor(
+    RandomForestRegressor(),
+    n_estimators=20,
+    n_jobs=-1,
+    random_state=337,
+    # bootstrap=True
+    bootstrap=False
+)
+# )
+# model= RandomForestRegressor()
+
+# 3. 훈련
+model.fit(x_train,y_train)
+
+# 4. 평가, 예측 
+y_pred = model.predict(x_test)
+print('model.score : ',model.score(x_test,y_test))
+print('r2_score : ',r2_score(y_test,y_pred))
+'''
+랜덤  포레스트 
+model.score :  0.7877188991827893
+r2_score :  0.7877188991827893
+'''
+'''
+배깅 -랜덤 포레스트 - bootstrap = False
+model.score :  0.785296011680072
+r2_score :  0.785296011680072
+'''
+'''
+배깅 -랜덤 포레스트 - bootstrap = True
+model.score :  0.7842851225720467
+r2_score :  0.7842851225720467
+'''
+'''
+배깅 - 배깅 -랜덤 포레스트 - bootstrap = True
+model.score :  0.7682040772740203
+r2_score :  0.7682040772740203
+'''
