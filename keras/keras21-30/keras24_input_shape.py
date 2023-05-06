d33207c6ddd4e7d1be6e0829e470ce9b96a51a72
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-05-06 13:21:53 +0900

    평가예측채워놓기

--- keras/keras21-30/keras24_input_shape.py
+++ keras/keras21-30/keras24_input_shape.py
@@ -8,10 +8,7 @@ from tensorflow.python.keras.layers import Dense
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler, StandardScalar #전처리
 from sklearn.preprocessing import MaxAbsScaler, RobustScaler #전처리
-# 일요일에 정규분포 같은 impot 거 정리해서 보내기 
-# Load the dataset
-# 메일 : 서너줄 정도 
-# 수식은 쓰지 말자 
+
 datasets = load_boston()
 x = datasets.data
 y = datasets.target
@@ -22,10 +19,12 @@ print(type(x))
 x_train, x_test, y_train, y_test = train_test_split(
     x,y,train_size=0.8,random_state=333    
 )
+
 # 모르는거 한 번 씩 돌려보기 
 scaler = StandardScalar() #X변환 
 #scaler = MinMaxScaler() #X변환 
 scaler.fit(x_train)  # 변환 기준
+
 #x_train 변환 범위에 맞춰서 변환해야한다. 
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test) 
@@ -49,11 +48,5 @@ model.compile(loss='mse',optimize='adam')
 
 #4. 평가, 예측
 
-# 우리는 수치로 예측 할 것인가
-# 모델 두 개로 돌리기! 
-# 두개의 데이터를 가지고 삼성전자으 ㅣ 주가르 맞출 것이다. 
-# 강도 습도 데이터 모아서 모델에 합쳐서 삼성전자의 주가를 맞출 것이다.
-# 시퀀셜로 가능 할까? 
 # 함수 = 재사용
-# 
  
