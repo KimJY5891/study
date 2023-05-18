e98bac3a5a4b440703852f3f1e16fbe658fe7ddd
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-05-18 15:44:34 +0900

    설명_autokeras01

---
+++ keras/keras61-70/keras62_autokeras1.py
@@ -0,0 +1,27 @@
+#자동화 머신러닝
+import autokeras as ak
+
+print(ak.__version__)
+
+import tensorflow as tf
+import time
+
+
+#1. 데이터 
+(x_train, y_train), (x_test, y_test) = \
+                            tf.keras.datasets.mnist.load_data()
+
+#2. 모델 
+model = ak.ImageClassifier(
+    overwrite = False,
+    max_trials=2
+)
+
+#3. 컴파일, 훈련
+start = time.time()
+model.fit(x_train, y_train, epochs = 10, validation_split = 0.15)
+end = time.time()
+
+#4. 평가, 예측 
+y_predict = model.predict(x_test)
+results = model.evaluate(x_test, y_test)
