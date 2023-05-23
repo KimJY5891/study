d33af2bd105891dd21808dcea9550bbd09061b3c
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-05-23 14:55:14 +0900

    설명_lambda

---
+++ keras/keras71-80/keras71_lamda.py
@@ -0,0 +1,12 @@
+#  2*x -4  :  리턴할 값 
+gradient1 = lambda x : 2*x -4
+
+def gradient2(x) : 
+    return 2*x - 4
+
+x = 3
+    
+print(gradient1) # <function <lambda> at 0x000001FC01E6F040>
+print(gradient1(3)) # 2
+print(gradient1(x)) # 2
+
