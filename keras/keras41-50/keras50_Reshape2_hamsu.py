f4e9ec35ea844a3f4a0623f3c9a16c012492a8d0
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-05-23 10:04:20 +0900

    설명_Reshape_hamsu

--- keras/keras41-50/keras50_Reshape2_hamsu.py
+++ keras/keras41-50/keras50_Reshape2_hamsu.py
@@ -1,22 +1,21 @@
-# 6만, 784,1 
-# 6만,28,28
+# 60000, 784, 1 
+# 60000, 28, 28
+import numpy as np
 from tensorflow.python.keras.models import Sequential,Input,Model
 from tensorflow.python.keras.layers import Dense, Conv2D, Flatten ,LSTM,MaxPooling2D,Conv1D, Reshape
-import numpy as np
 from sklearn.metrics import r2_score, accuracy_score
 from sklearn.preprocessing  import MinMaxScaler
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.datasets import mnist
-from sklearn.preprocessing import MinMaxScaler, StandardScaler #전처리
-from sklearn.preprocessing import MaxAbsScaler, RobustScaler #전처리
+from sklearn.preprocessing import MinMaxScaler, StandardScaler,rMaxAbsScaler, RobustScaler #전처리
 
 # 1. 데이터
 (x_train,y_train),(x_test,y_test) = mnist.load_data() # 데이터를 넣어줌 
 x_train=x_train.reshape(60000,28,28,1)/255.
 x_test=x_test.reshape(10000,28,28,1)/255.
 
-#스케일링
 
+#스케일링
 print("x_train.shape : ",x_train.shape)#(60000, 28, 28, 1)
 print("y_train.shape : ",y_train.shape) #y_train.shape :  (60000,)
 
@@ -51,9 +50,6 @@ model=Model(inputs=Input01,outputs=Output01)
 model.summary()
 
 '''
-
-
-
 
 #3.컴파일,훈련
 
