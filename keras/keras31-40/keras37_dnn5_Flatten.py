79005c9297d391172ac4486d0a418b4a666c9d5d
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-05-29 01:15:08 +0900

    설명_Flatten

--- keras/keras31-40/keras37_dnn5_Flatten.py
+++ keras/keras31-40/keras37_dnn5_Flatten.py
@@ -1,15 +1,12 @@
-import tensorflow as tf
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from keras.utils import to_categorical
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler #전처리
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Conv2D, Flatten
-import numpy as np
 from sklearn.metrics import r2_score, accuracy_score
-from sklearn.preprocessing  import MinMaxScaler
-from keras.utils import to_categorical
-from tensorflow.keras.datasets import mnist
-from sklearn.preprocessing import MinMaxScaler, StandardScaler #전처리
-from sklearn.preprocessing import MaxAbsScaler, RobustScaler #전처리
 
-# 1. epdlxj 
+# 1. 데이터 
 (x_train,y_train),(x_test,y_test) = mnist.load_data() # 데이터를 넣어줌 
 
 scaler = MinMaxScaler()
@@ -38,7 +35,7 @@ model.add(Dense(10, activation='softmax'))
 model.summary() 
 #3차원받아들이고 2차원으로 바꿔줘야한다. 
 #flatten으로 변화 해주면 된다.
-# 
+
 '''
 #3.컴파일,훈련
 model.compile(loss='categorical_crossentropy',optimizer='adam')
