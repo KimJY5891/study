9e5e79f442eb3b97c149f4f50cbc55c35356d767
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-05-28 22:13:40 +0900

    설명_optimizer과ReduceLR + 결과치 확인하기

--- keras/keras61-70/keras64_optimizer.py
+++ keras/keras61-70/keras64_optimizer.py
@@ -27,17 +27,18 @@ model.add(Dense(1))
 # model.compile(loss='mse ',optimizer='adam') # 러닝레이트 디폴트 값 찾기 
 from tensorflow.keras.optimizers import Adam
 learning_rate = 0.1
-optimizer = Adam(learning_rate=learning_rate)
-model.compile(loss='mse ',optimizer=optimizer) # 러닝레이트 디폴트 값 찾기 
-# 10까지 개선이 없으면 러닝레이트를 줄여버리겠다. ->
+optimizer = Adam(learning_rate=learning_rate) # Adam optimizer의 learning_rate 디폴트 값 =  0.001 
+model.compile(loss='mse ',optimizer=optimizer) 
+'''
+2번째 방법
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau # 러닝레이트를 감축시켜라 
 es = EarlyStopping(monitor = 'val_loss',patience = 20, mode = 'min', verbose = 1)
-es = ReduceLROnPlateau(monitor='val_loss', patience= 10,mode= 'auto',verbose=1, factor=0.5 # 반띵 해주기
-                       ) # 얼리스타핑이랑 리듀스엘알이 같이사용할 때 수가 같으면 리듀스엘알이 안먹ㅇ힌다. 먹히기전에 얼릴스탑핑으로 끝나기 때문 
-# 덩어리가 더 큰 놈들이 잘 먹힌다. 0<-<
-model.fit(x_train, y_train, epochs=10, batch_size = 32,verbose = 1, validation_split=0.2, callbacks=[es,])
-
-
+rlr = ReduceLROnPlateau(monitor='val_loss', patience= 10,# 10까지 개선이 없으면 러닝레이트를 줄여버리겠다.
+                        mode= 'auto',verbose=1, factor=0.5 # 반띵 해주기
+                       )# EarlyStopping이랑 ReduceLR이 같이 사용할 때 수가 같으면 ReduceLR이 안 먹힌다. 먹히기 전에 EarlyStopping으로 끝나기 때문이다.  
+# 덩어리가 더 큰 놈들이 잘 먹힌다.
+model.fit(x_train, y_train, epochs=10, batch_size = 32,verbose = 1, validation_split=0.2, callbacks=[es,rlr])
+'''
 # 4. 평가, 예측
 
 result = model.evaluate(x_test,y_test)  
