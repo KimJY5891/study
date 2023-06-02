2ac61bac92f327c73783a8b50efc857c466e36ba
kim ji yoon <126533174+kjy6612@users.noreply.github.com>
2023-06-02 20:56:14 +0900

    연습

--- AE/AE01~10/a04_ae1_many_graph.py
+++ AE/AE01~10/a04_ae1_many_graph.py
@@ -1,66 +1,100 @@
-#LSTM
-# rnn의단 점을 보완 
-# 너무 길어져서 먼위치에 있는 정보를 기억할 수 없다는 단점을 보완 
-# rnn의 개량형 통상적으로 4배의 연산을 더한다. 
-# 배니싱 = 사라진다.는 의미
-# 처음 스테이스값이 마지막가지 못간다. 
-# 하지만 어느정도 지나면 기억하려는 것도 어느 정도 망각된다. 
-# rnn 만들어 보라고 하면 LSTM을 만들게 된다. 가장 성능이 좋다. 
-# 심플RNN보다 LSTM이 성능이 더좋다. 
-
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, SimpleRNN,Dropout, LSTM
+from tensorflow.keras.datasets import mnist
 
-# 1. 데티ㅓ 
-datasets = np.array([1,2,3,4,5,6,7,8,9,10])
-#y=?
-#시계열 은 y가 없다. 
-x= np.array([[1,2,3,4,5],[2,3,4,5,6],[3,4,5,6,7],[4,5,6,7,8],[5,6,7,8,9]])
-#8.9.10 넣으면 y 데이터를 만들수 없다. 
-y = np.array([6,7,8,9,10])
-print(x.shape,y.shape)#(7, 3) (7,)
-# x의shape = (행,열,몇개씩 훈련하는지)
-# 3차원 데이터라 reshape 해줘야한다.
-x=x.reshape(5,5,1)
-print(x)#[[[1],[2],[3]],[[2],[3],[4]],[[3],[4],[5]],[[4],[5],[6]], [[5],[6] ...
-# dnn = 2차원 , cnn = 4차원, rnn=3c차원
-print(x.shape) #(5, 5, 1)
 
-#2.모델 구성
-model=Sequential()
-model.add(LSTM(200,input_shape=(5,1))) #input 모두가 행빼고 나머지 이듯 
-model.add(Dropout(0.5))
-model.add(Dense(8,activation='relu'))
-model.add(Dense(16,activation='relu'))
-model.add(Dense(32))
-model.add(Dense(64,activation='relu'))
-model.add(Dense(128,activation='relu'))
-model.add(Dense(256,activation='relu'))
-model.add(Dropout(0.3))
-model.add(Dense(128,activation='relu'))
-model.add(Dense(64,activation='relu'))
-model.add(Dense(16))
-model.add(Dense(4))
-model.add(Dense(1))
+# 1. 데이터
+(x_train, _), (x_test, _) = mnist.load_data()
 
-#3. 컴파일, 훈련 
-model.compile(loss='mse',optimizer='adam')
-import time
-start = time.time()
-# from tensorflow.python.keras.callbacks import EarlyStopping
-# es = EarlyStopping(monitor='loss',patience=40,mode='min',verbose=1,restore_best_weights=True)
-model.fit(x,y,epochs=1000,batch_size=100)
-end = time.time()
-#4. 평가 예측
-loss =model.evaluate(x,y)
-x_predict = np.array([6,7,8,9,10]).reshape(1,5,1) #[[6],[7],[8],[9],[10]]
-print(x_predict.shape)
-print(x_predict)
-# 벡터 한개는 1차원 
-result = model.predict(x_predict)
-print('loss : ',loss)
-print('[[6],[7],[8],[9],[10]]의 결과 : ',result)
-print('걸린시간 : ',round(end-start,2))
+x_train = x_train.reshape(60000, 784).astype('float32')/255.
+x_test = x_test.reshape(10000, 784).astype('float32')/255.
 
-# [[6],[7],[8],[9],[10]]의 결과 :
+x_train_noised = x_train + np.random.normal(0, 0.5, size = x_train.shape) #정규분포 형식의 임의의 값
+x_test_noised = x_test + np.random.normal(0, 0.5, size = x_test.shape)
+
+print(x_train_noised.shape, x_test_noised.shape)
+
+print(np.max(x_train_noised), np.min(x_train_noised))
+print(np.max(x_test_noised), np.min(x_test_noised))
+
+
+x_train_noised = np.clip(x_train_noised, a_min = 0, a_max = 1)
+x_test_noised = np.clip(x_test_noised, a_min = 0, a_max = 1) #최저값 0 최고값 1로 고정시키는 함수
+
+print(np.max(x_train_noised), np.min(x_train_noised))
+print(np.max(x_test_noised), np.min(x_test_noised))
+
+
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Input
+
+def autoencoder(hidden_layer_size):
+    model = Sequential()
+    model.add(Dense(units = hidden_layer_size, input_shape = (784,)))
+    model.add(Dense(784, activation='sigmoid'))
+    return model
+
+model_001 = autoencoder(hidden_layer_size=1) #PCA 100% 성능
+model_008 = autoencoder(hidden_layer_size=8) #PCA 100% 성능
+model_032 = autoencoder(hidden_layer_size=32) #PCA 100% 성능
+model_064 = autoencoder(hidden_layer_size=64) #PCA 100% 성능
+model_154 = autoencoder(hidden_layer_size=154) #PCA 100% 성능
+model_331 = autoencoder(hidden_layer_size=331) #PCA 99% 성능
+model_486 = autoencoder(hidden_layer_size=486) #PCA 99.9% 성능
+model_713 = autoencoder(hidden_layer_size=713) #PCA 100% 성능
+
+# 3. 컴파일, 훈련
+
+# for i,v in enumerate
+print('========================== node 1개 시작 ============================')
+model_001.compile(optimizer = 'adam', loss = 'mse', )
+model_001.fit(x_train_noised, x_train, epochs = 10, batch_size = 128, validation_split = 0.2)
+print('========================== node 8개 시작 ============================')
+model_008.compile(optimizer = 'adam', loss = 'mse', )
+model_008.fit(x_train_noised, x_train, epochs = 10, batch_size = 128, validation_split = 0.2)
+print('========================== node 32개 시작 ============================')
+model_032.compile(optimizer = 'adam', loss = 'mse', )
+model_032.fit(x_train_noised, x_train, epochs = 10, batch_size = 128, validation_split = 0.2)
+print('========================== node 64개 시작 ============================')
+model_064.compile(optimizer = 'adam', loss = 'mse', )
+model_064.fit(x_train_noised, x_train, epochs = 10, batch_size = 128, validation_split = 0.2)
+print('========================== node 154개 시작 ============================')
+model_154.compile(optimizer = 'adam', loss = 'mse', )
+model_154.fit(x_train_noised, x_train, epochs = 10, batch_size = 128, validation_split = 0.2)
+print('========================== node 331개 시작 ============================')
+model_331.compile(optimizer = 'adam', loss = 'mse', )
+model_331.fit(x_train_noised, x_train, epochs = 10, batch_size = 128, validation_split = 0.2)
+print('========================== node 486개 시작 ============================')
+model_486.compile(optimizer = 'adam', loss = 'mse', )
+model_486.fit(x_train_noised, x_train, epochs = 10, batch_size = 128, validation_split = 0.2)
+print('========================== node 713개 시작 ============================')
+model_713.compile(optimizer = 'adam', loss = 'mse', )
+model_713.fit(x_train_noised, x_train, epochs = 10, batch_size = 128, validation_split = 0.2)
+
+# 4. 평가, 예측
+
+# decoded_imgs_ = np.round(autoencoder.predict(x_test))
+decoded_imgs_001 = model_001.predict(x_test_noised)
+decoded_imgs_008 = model_008.predict(x_test_noised)
+decoded_imgs_032 = model_032.predict(x_test_noised)
+decoded_imgs_064 = model_064.predict(x_test_noised)
+decoded_imgs_154 = model_154.predict(x_test_noised)
+decoded_imgs_331 = model_331.predict(x_test_noised)
+decoded_imgs_486 = model_486.predict(x_test_noised)
+decoded_imgs_713 = model_713.predict(x_test_noised)
+
+############################# 아무 생각 없이 타자 연습 ######################
+import matplotlib.pyplot as plt
+import random
+
+fig, axes = plt.subplots(7,5,figsize=(15,15))
+random_images = random.sample(range(decoded_imgs_001.shape[0]),5)
+outputs = [x_test,decoded_imgs_001,decoded_imgs_008,decoded_imgs_032,decoded_imgs_064,decoded_imgs_154,decoded_imgs_331,
+           decoded_imgs_486,decoded_imgs_713
+           ]
+for row_num, row in enumerate(axes) : 
+        for col_num, ax in enumerate(row) : 
+            ax.imshow(outputs[row_num][random_images[col_num]].reshape(28,28),cmap='gray')
+            ax.grid(False)
+            ax.set_xticks([])
+            ax.set_yticks([])
+plt.show()
