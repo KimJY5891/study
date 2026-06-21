80f9e96ea1a651a44276b1e44679a5dd67d1d212
kim ji yoon <126533174+KimJY5891@users.noreply.github.com>
2026-06-22 01:51:36 +0900

    입출력

---
+++ c/c_study_03_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	char c = 'A';
+	int i = 10, j = 20, k = 30;
+	printf("간단한 출력 프로그램\n");
+	printf("'%c'의 ASCII 코드값은 %d\n",c,c);
+	printf("i=%d,j=%d, k=%d\n",i,j,k);
+}
+/* 간단한 출력 프로그램
+'A'의 ASCII 코드값은 65
+i=10,j=20, k=30 */
